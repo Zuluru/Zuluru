8e721d90a6ba180bccaad42f1c06ff39d0023dc8
gregs <git@zuluru.org>
2012-10-19 15:06:32 -0400

    Implement query functionality for using MEMBER_TYPE rule in mailing lists

--- controllers/components/rule_member_type.php
+++ controllers/components/rule_member_type.php
@@ -34,10 +34,58 @@ class RuleMemberTypeComponent extends RuleComponent
 		return ($intro ? 'intro' : 'none');
 	}
 
-	function build_query($affiliate, &$joins, &$fields) {
-		// TODO: Membership start and end dates are in the custom data,
-		// making queries that use those values very difficult
-		return false;
+	function build_query($affiliate, &$joins, &$fields, &$conditions) {
+		if (!isset($this->events)) {
+			$date = date('Y-m-d', strtotime ($this->config));
+			$model = ClassRegistry::init('Event');
+			$types = $model->EventType->find('list', array(
+					'contain' => array(),
+					'conditions' => array('type' => 'membership'),
+			));
+			$events = $model->find('all', array(
+					'contain' => array(),
+					'conditions' => array(
+						'Event.event_type_id' => array_keys($types),
+						'Event.affiliate_id' => $affiliate,
+					),
+			));
+			foreach ($events as $key => $event) {
+				if ($event['Event']['membership_begins'] > $date || $event['Event']['membership_ends'] < $date) {
+					unset($events[$key]);
+				}
+			}
+			$this->events = Set::extract('/Event/id', $events);
+		}
+
+		$joins['Registration'] = array(
+			'table' => 'registrations',
+			'alias' => 'Registration',
+			'type' => 'LEFT',
+			'foreignKey' => false,
+			'conditions' => 'Registration.person_id = Person.id',
+		);
+		$joins['Event'] = array(
+			'table' => 'events',
+			'alias' => 'Event',
+			'type' => 'LEFT',
+			'foreignKey' => false,
+			'conditions' => 'Event.id = Registration.event_id',
+		);
+
+		$conditions['Event.id'] = $this->events;
+		$conditions['Registration.payment'] = 'Paid';
+
+		if (Configure::read('feature.affiliate')) {
+			$conditions['Event.affiliate_id'] = $affiliate;
+		}
+
+		// TODO: This is almost certainly MySQL-specific
+		$type_str = '"membership_type";s:';
+		$type_len_pos = "POSITION('$type_str' IN custom) + " . strlen($type_str);
+		$type_len_len = "POSITION('\"' IN SUBSTR(custom, $type_len_pos))";
+		$type_len = "SUBSTR(custom, $type_len_pos, $type_len_len)";
+		$type = "SUBSTR(custom, $type_len_pos + $type_len_len, $type_len)";
+		return $type;
 	}
 
 	function desc() {
