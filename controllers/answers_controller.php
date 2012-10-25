f42f4ebb57ed58a82589639497ccc5b186dd191b
gregs <git@zuluru.org>
2012-10-25 14:38:10 -0400

    Manager permission fix

--- controllers/answers_controller.php
+++ controllers/answers_controller.php
@@ -2,6 +2,29 @@
 class AnswersController extends AppController {
 
 	var $name = 'Answers';
+	var $uses = array('Answer', 'Question');
+
+	function isAuthorized() {
+		if ($this->is_manager) {
+			// Managers can perform these operations in affiliates they manage
+			if (in_array ($this->params['action'], array(
+					'activate',
+					'deactivate',
+			)))
+			{
+				// If an answer id is specified, check if we're a manager of that answer's affiliate
+				$answer = $this->_arg('answer');
+				if ($answer) {
+					$question = $this->Answer->field('question_id', array('Answer.id' => $answer));
+					if (in_array($this->Question->affiliate($question), $this->Session->read('Zuluru.ManagedAffiliateIDs'))) {
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
 
 	function activate() {
 		Configure::write ('debug', 0);
