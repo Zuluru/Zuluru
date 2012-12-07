f68a21ae236b409718b4eef11afc5678599215b4
gregs <git@zuluru.org>
2012-12-07 16:37:04 -0500

    Add missing default affiliate data

---
+++ config/schema/data/affiliates_data.php
@@ -0,0 +1,15 @@
+<?php
+class AffiliatesData {
+
+	public $table = 'affiliates';
+
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Club',
+			'active' => 1,
+		),
+	);
+
+}
+?>
---
+++ config/schema/data/affiliates_people_data.php
@@ -0,0 +1,14 @@
+<?php
+class AffiliatesPeopleData {
+
+	public $table = 'affiliates_people';
+
+	public $records = array(
+		array(
+			'affiliate_id' => 1,
+			'person_id' => 1,
+		),
+	);
+
+}
+?>
