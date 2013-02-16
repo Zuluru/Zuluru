5b2d5c866a0fadefe58428c963dc813319bbb98a
gregs <git@zuluru.org>
2013-02-16 01:33:28 -0500

    Changes missed in previous update

--- webroot/js/sport_baseball.js
+++ webroot/js/sport_baseball.js
@@ -18,12 +18,17 @@ function layoutText(id)
 			'<br>Outfield: ' + fields[id].length * 3 + ' feet';
 }
 
+function mound(width)
+{
+	return width * 60.5 / 90;
+}
+
 function outlinePositions(id)
 {
 	var position = fields[id].marker.getPosition();
 
 	var bb = new Array;
-	var home = makePosition(position, fields[id].length / 2, 270 - fields[id].angle);
+	var home = makePosition(position, mound(fields[id].width), 270 - fields[id].angle);
 	bb[0] = home;
 	bb[1] = makePosition(bb[0], fields[id].length, 45 - fields[id].angle);
 	bb = bb.concat(makeArc(home, 45 - fields[id].angle, 135 - fields[id].angle, fields[id].length));
@@ -33,15 +38,17 @@ function outlinePositions(id)
 
 function inlinePositions(id)
 {
+	var position = fields[id].marker.getPosition();
+
 	var bb = new Array;
-	var home = makePosition(position, fields[id].length / 2, 270 - fields[id].angle);
+	var home = makePosition(position, mound(fields[id].width), 270 - fields[id].angle);
 
 	bb[0] = new Array;
 	bb[0][0] = makePosition(home, fields[id].width, 45 - fields[id].angle);
 	bb[0][1] = makePosition(bb[0][0], fields[id].width, 135 - fields[id].angle);
 	bb[0][2] = makePosition(bb[0][1], fields[id].width, 225 - fields[id].angle);
 
-	bb[1] = makeArc(makePosition(home, fields[id].width * 0.667, 90 - fields[id].angle), 19 - fields[id].angle, 161 - fields[id].angle, fields[id].width * 95 / 90);
+	bb[1] = makeArc(makePosition(home, mound(fields[id].width), 90 - fields[id].angle), 19 - fields[id].angle, 161 - fields[id].angle, fields[id].width * 95 / 90);
 
 	return bb;
 }
