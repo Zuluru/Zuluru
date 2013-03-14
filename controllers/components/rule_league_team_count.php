76bcca0920ba434594b7fdf5b199f759859a17e1
gregs <git@zuluru.org>
2013-03-14 17:59:22 -0400

    Add option for including subs in league team counts

--- controllers/components/rule_league_team_count.php
+++ controllers/components/rule_league_team_count.php
@@ -8,27 +8,35 @@ class RuleLeagueTeamCountComponent extends RuleComponent
 	var $query_having = 'Person.id HAVING team_count';
 
 	function parse($config) {
-		$config = trim ($config, '"\'');
-		$this->config = array_map ('trim', explode (',', $config));
+		$config = str_replace(array('"', "'"), '', low($config));
+		$this->config = array(
+			'leagues' => array_map ('trim', explode (',', $config)),
+		);
+		$sub_key = array_search('include_subs', $this->config['leagues']);
+		if ($sub_key !== false) {
+			$this->config['roles'] = Configure::read('extended_playing_roster_roles');
+			unset($this->config['leagues'][$sub_key]);
+		} else {
+			$this->config['roles'] = Configure::read('playing_roster_roles');
+		}
 		return true;
 	}
 
 	// Count how many teams the user was on in the given leagues.
-	// Since we're only interested in non-subs, if the user in
+	// If we're only interested in non-subs, if the user in
 	// question is a sub on the current team, we'll just return 0.
 	function evaluate($affiliate, $params, $team) {
 		$count = 0;
-		$roles = Configure::read('playing_roster_roles');
 
 		$role = Set::extract("/Person[id={$params['Person']['id']}]", $team);
-		if (!empty($role) && !array_key_exists($role[0]['Person']['TeamsPerson']['role'], $roles)) {
+		if (!empty($role) && !array_key_exists($role[0]['Person']['TeamsPerson']['role'], $this->config['roles'])) {
 			return 0;
 		}
 
 		foreach ($params['Team'] as $team) {
-			if (in_array($team['TeamsPerson']['role'], $roles) &&
+			if (in_array($team['TeamsPerson']['role'], $this->config['roles']) &&
 				$team['TeamsPerson']['status'] == ROSTER_APPROVED &&
-				in_array($team['Division']['League']['id'], $this->config))
+				in_array($team['Division']['League']['id'], $this->config['leagues']))
 			{
 				++ $count;
 			}
@@ -60,8 +68,8 @@ class RuleLeagueTeamCountComponent extends RuleComponent
 			'conditions' => 'Division.id = Team.division_id',
 		);
 		return array(
-			'Division.league_id' => $this->config,
-			'TeamsPerson.role' => Configure::read('playing_roster_roles'),
+			'Division.league_id' => $this->config['leagues'],
+			'TeamsPerson.role' => $this->config['roles'],
 			'TeamsPerson.status' => ROSTER_APPROVED,
 		);
 	}
--- views/elements/help/rules/rules/league_team_count.ctp
+++ views/elements/help/rules/rules/league_team_count.ctp
@@ -1,10 +1,12 @@
 <?php $year = date('Y'); ?>
 <h4>Type: Data</h4>
 <p>The LEAGUE_TEAM_COUNT rule accepts a comma-separated list of league ids and returns a count of how many teams the player is/was on that play/played in those leagues.</p>
-<p>Only teams where the player is listed as a captain, assistant captain or regular player, and is accepted on the roster, are counted.</p>
+<p>By default, only teams where the player is listed as a captain, assistant captain or regular player, and is accepted on the roster, are counted. You can also include teams where the player is listed as a substitute by including 'include_subs' anywhere in the league id list.</p>
 <p>Note that this looks at all divisions within the specified leagues.</p>
 <p>Example:</p>
 <pre>LEAGUE_TEAM_COUNT(123)</pre>
 <p>would return the number of teams playing in league #123 that the player is on.</p>
 <pre>LEAGUE_TEAM_COUNT(123,124)</pre>
 <p>would return the number of teams playing in leagues #123 or 124 that the player is on.</p>
+<pre>LEAGUE_TEAM_COUNT(123,124,include_subs)</pre>
+<p>would return the number of teams playing in leagues #123 or 124 that the player is on, even as a substitute.</p>
