09cd1427423a3707b61e29df63f0727d54d71c27
gregs <git@zuluru.org>
2013-04-04 14:20:46 -0400

    New version number for league/division improvements

--- config/version.php
+++ config/version.php
@@ -6,7 +6,7 @@
 if (!defined('ZULURU_VERSION')) {
 	define('ZULURU_MAJOR', 1);
 	define('ZULURU_MINOR', 6);
-	define('ZULURU_REVISION', 3);
+	define('ZULURU_REVISION', 4);
 	define('SCHEMA_VERSION', 36);
 }
 
