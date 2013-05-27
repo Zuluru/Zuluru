17223b066bc3865119b7cc096f4fec49746fd54c
gregs <git@zuluru.org>
2013-05-27 13:48:39 -0400

    Remove obsolete connection to affiliates

--- models/facility.php
+++ models/facility.php
@@ -11,12 +11,6 @@ class Facility extends AppModel {
 				'message' => 'Name cannot be empty',
 			),
 		),
-		'affiliate_id' => array(
-			'inlist' => array(
-				'rule' => array('inquery', 'Affiliate', 'id'),
-				'message' => 'You must select a valid affiliate.',
-			),
-		),
 		'code' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
@@ -35,13 +29,6 @@ class Facility extends AppModel {
 	);
 
 	var $belongsTo = array(
-		'Affiliate' => array(
-			'className' => 'Affiliate',
-			'foreignKey' => 'affiliate_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
 		'Region' => array(
 			'className' => 'Region',
 			'foreignKey' => 'region_id',
