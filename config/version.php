d8793ea13198e6823c54f80acf3b0feba306ea1c
gregs <git@zuluru.org>
2013-07-20 20:29:52 -0400

    Update database schema

--- config/version.php
+++ config/version.php
@@ -7,7 +7,7 @@ if (!defined('ZULURU_VERSION')) {
 	define('ZULURU_MAJOR', 1);
 	define('ZULURU_MINOR', 6);
 	define('ZULURU_REVISION', 10);
-	define('SCHEMA_VERSION', 48);
+	define('SCHEMA_VERSION', 49);
 }
 
 ?>
