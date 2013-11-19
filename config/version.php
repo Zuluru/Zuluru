c562be5b31184a7a6522a9f8f6b7365bc7810695
gregs <git@zuluru.org>
2013-11-18 21:45:33 -0500

    Updated version number

--- config/version.php
+++ config/version.php
@@ -6,7 +6,7 @@
 if (!defined('ZULURU_VERSION')) {
 	define('ZULURU_MAJOR', 1);
 	define('ZULURU_MINOR', 6);
-	define('ZULURU_REVISION', 14);
+	define('ZULURU_REVISION', 15);
 	define('SCHEMA_VERSION', 51);
 }
 
