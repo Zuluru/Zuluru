b2edb960d8a22ddae6e42db0638042c67d264fa5
gregs <git@zuluru.org>
2014-01-09 16:30:08 -0500

    Cache duplicate calculation and person record; rename some variables

--- controllers/components/can_register.php
+++ controllers/components/can_register.php
@@ -1,57 +1,66 @@
 <?php
 class CanRegisterComponent extends Object
 {
+	// Cached versions of some data, for when we call test() again and again
+	var $person = null;
+	var $person_id = null;
+	var $person_duplicates = null;
+
 	function initialize(&$controller, $settings = array()) {
 		// Save the controller reference for later use
-		$this->controller =& $controller;
+		$this->_controller =& $controller;
 	}
 
 	/**
 	 * Test whether a user is allowed to register for something
 	 *
-	 * @param mixed $user_id User id
+	 * @param mixed $person_id Person id
 	 * @param mixed $event Event id
 	 * @param mixed $ignore_date When adding preregistrations, we must be able to ignore the date
 	 * @param mixed $strict If false, we will allow things with prerequisites that are not yet filled but can easily be
 	 * @param mixed $waiting If true, we will ignore the cap to allow waiting list registrations
 	 * @return mixed True if the user can register for the event
 	 */
-	function test($user_id, $event, $ignore_date = false, $strict = true, $waiting = false) {
+	function test($person_id, $event, $ignore_date = false, $strict = true, $waiting = false) {
 		if (!isset ($this->Html)) {
 			App::import ('helper', 'Html');
 			$this->Html = new HtmlHelper();
 		}
 
 		// Get everything from the user record that the decisions below might need
-		if (!isset ($this->controller->Person)) {
-			$this->controller->Person = ClassRegistry::init ('Person');
+		if (!isset ($this->_controller->Person)) {
+			$this->_controller->Person = ClassRegistry::init ('Person');
 		}
 
-		$person = array(
-			'Person' => $this->controller->UserCache->read('Person', $user_id),
-			'Team' => $this->controller->UserCache->read('Teams', $user_id),
-			'Preregistration' => $this->controller->UserCache->read('Preregistrations', $user_id),
-			'Registration' => array_merge(
-				$this->controller->UserCache->read('RegistrationsPaid', $user_id),
-				$this->controller->UserCache->read('RegistrationsUnpaid', $user_id)
-			),
-			'Upload' => $this->controller->UserCache->read('Documents', $user_id),
-			'Affiliate' => $this->controller->UserCache->read('Affiliates', $user_id),
-			'Waiver' => $this->controller->UserCache->read('Waivers', $user_id),
-		);
+		if ($person_id != $this->person_id) {
+			$this->person_id = $person_id;
+			$this->person = array(
+				'Person' => $this->_controller->UserCache->read('Person', $person_id),
+				'Team' => $this->_controller->UserCache->read('Teams', $person_id),
+				'Preregistration' => $this->_controller->UserCache->read('Preregistrations', $person_id),
+				'Registration' => array_merge(
+					$this->_controller->UserCache->read('RegistrationsPaid', $person_id),
+					$this->_controller->UserCache->read('RegistrationsUnpaid', $person_id)
+				),
+				'Upload' => $this->_controller->UserCache->read('Documents', $person_id),
+				'Affiliate' => $this->_controller->UserCache->read('Affiliates', $person_id),
+				'Waiver' => $this->_controller->UserCache->read('Waivers', $person_id),
+			);
+			$this->person_duplicates = null;
+		}
 
 		// Pull out the registration record(s) for the current event, if any.
-		$registrations = Set::extract ("/Event[id={$event['Event']['id']}]/..", $person['Registration']);
+		$registrations = Set::extract ("/Event[id={$event['Event']['id']}]/..", $this->person['Registration']);
 		$is_registered = !empty ($registrations);
 
 		// Check the registration rule, if any
 		if (!empty ($event['Event']['register_rule'])) {
 			$rule_obj = AppController::_getComponent ('Rule');
 			if (!$rule_obj->init ($event['Event']['register_rule'])) {
-				$this->controller->Session->setFlash(__('Failed to parse the rule', true), 'default', array('class' => 'error'));
+				$this->_controller->Session->setFlash(__('Failed to parse the rule', true), 'default', array('class' => 'error'));
 			}
 
-			$rule_allowed = $rule_obj->evaluate($event['Event']['affiliate_id'], $person, null, $strict, false);
+			$rule_allowed = $rule_obj->evaluate($event['Event']['affiliate_id'], $this->person, null, $strict, false);
 		}
 
 		// Find the registration cap and how many are already registered.
@@ -60,24 +69,26 @@ class CanRegisterComponent extends Object
 			'payment' => array('Paid', 'Pending'),
 		);
 		if ($event['Event']['cap_female'] != -2) {
-			$conditions['gender'] = $person['Person']['gender'];
+			$conditions['gender'] = $this->person['Person']['gender'];
 		}
 
-		$cap = Event::cap($event['Event']['cap_male'], $event['Event']['cap_female'], $person['Person']['gender']);
-		if (!isset ($this->controller->Event)) {
-			$this->controller->Event = ClassRegistry::init ('Event');
+		$cap = Event::cap($event['Event']['cap_male'], $event['Event']['cap_female'], $this->person['Person']['gender']);
+		if (!isset ($this->_controller->Event)) {
+			$this->_controller->Event = ClassRegistry::init ('Event');
 		}
 		if ($cap != -1) {
-			$this->controller->Event->Registration->contain ('Person');
-			$paid = $this->controller->Event->Registration->find ('count', array('conditions' => $conditions));
+			$this->_controller->Event->Registration->contain ('Person');
+			$paid = $this->_controller->Event->Registration->find ('count', array('conditions' => $conditions));
 		}
 
 		// Check whether this user is considered active for the purposes of registration
-		$is_active = ($this->controller->UserCache->read('Person.status') == 'active');
+		$is_active = ($this->person['Person']['status'] == 'active');
 		// If the user is not yet approved, we may let them register but not pay
-		if ($this->controller->UserCache->read('Person.status') == 'new' && Configure::read('registration.allow_tentative')) {
-			$duplicates = $this->controller->Person->findDuplicates ($person);
-			if (empty ($duplicates)) {
+		if ($this->person['Person']['status'] == 'new' && Configure::read('registration.allow_tentative')) {
+			if ($this->person_duplicates === null) {
+				$this->person_duplicates = $this->_controller->Person->findDuplicates ($this->person);
+			}
+			if (empty ($this->person_duplicates)) {
 				$is_active = true;
 			}
 		}
@@ -117,10 +128,10 @@ class CanRegisterComponent extends Object
 		}
 
 		// If there is a preregistration record, we ignore open and close times.
-		$prereg = Set::extract ("/Preregistration[event_id={$event['Event']['id']}]", $person['Preregistration']);
+		$prereg = Set::extract ("/Preregistration[event_id={$event['Event']['id']}]", $this->person['Preregistration']);
 		if (empty ($prereg) && !$ignore_date) {
 			// Admins can test registration before it opens...
-			if (!$this->controller->is_admin) {
+			if (!$this->_controller->is_admin) {
 				if (strtotime ($event['Event']['open']) + Configure::read('timezone.adjust') * 60 > time()) {
 					$messages[] = array('text' => __('This event is not yet open for registration.', true), 'class' => 'closed');
 					$continue = false;
