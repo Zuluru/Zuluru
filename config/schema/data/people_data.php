6233d33f5ac0a8ac841211053c6376809737690f
gregs <git@zuluru.org>
2014-01-31 16:57:01 -0500

    Correct initial data for people and users tables

--- config/schema/data/people_data.php
+++ config/schema/data/people_data.php
@@ -5,11 +5,10 @@ class PeopleData {
 
 	public $records = array(
 		array(
-			'user_name' => 'admin',
-			'password' => 'c054b152596745efa1d197b809fa7fc70ce586e5',
+			'id' => 1,
+			'user_id' => 1,
 			'first_name' => 'Administrator',
 			'last_name' => '',
-			'email' => 'you@example.com',
 			'publish_email' => 1,
 			'group_id' => 3,
 			'status' => 'active',
---
+++ config/schema/data/user_data.php
@@ -0,0 +1,16 @@
+<?php
+class UserData {
+
+	public $table = 'users';
+
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_name' => 'admin',
+			'password' => 'c054b152596745efa1d197b809fa7fc70ce586e5',
+			'email' => 'you@example.com',
+		),
+	);
+
+}
+?>
